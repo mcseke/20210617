d6f9d348afd60fd593c10b1c6d3b4f51f1ac7010
Marton Cseke <marton.cseke@urss.hu>
2021-06-18 14:15:19 +0200

    labs

--- apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import {Component, OnInit} from '@angular/core';
-import {FlightService} from '@flight-workspace/flight-lib';
+import { Flight, FlightService } from '@flight-workspace/flight-lib';
+import { FlightBookingAppState } from '../+state/flight-booking.reducer';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { flightsLoaded, loadFlights, updateFlight } from '../+state/flight-booking.actions';
+import { take } from 'rxjs/operators';
+import { selectedFilteredFlights } from '../+state/flight-booking.selectors';
 
 @Component({
   selector: 'flight-search',
@@ -14,6 +20,8 @@ export class FlightSearchComponent implements OnInit {
   to = 'Graz'; // in Austria
   urgent = false;
 
+  flights$: Observable<Flight[]>;
+
   get flights() {
     return this.flightService.flights;
   }
@@ -25,21 +33,49 @@ export class FlightSearchComponent implements OnInit {
   };
 
   constructor(
-    private flightService: FlightService) {
+    private flightService: FlightService,
+    private store: Store<FlightBookingAppState>) {
   }
 
   ngOnInit() {
+    console.log('onInit');
+    this.flights$ = this.store.select(selectedFilteredFlights);
   }
 
   search(): void {
     if (!this.from || !this.to) return;
 
-    this.flightService
-      .load(this.from, this.to, this.urgent);
+    // New:
+    this.store.dispatch(loadFlights({
+      from: this.from,
+      to: this.to,
+      urgent: this.urgent
+    }));
+
+
+    // this.flightService
+    //   .find(this.from, this.to, this.urgent)
+    //   .subscribe(
+    //     flights => {
+    //       this.store.dispatch(flightsLoaded({flights}));
+    //     },
+    //     error => {
+    //       console.error('error', error);
+    //     }
+    //   );
   }
 
   delay(): void {
-    this.flightService.delay();
+
+    this.flights$.pipe(take(1)).subscribe(flights => {
+      const flight = flights[0];
+
+      const oldDate = new Date(flight.date);
+      const newDate = new Date(oldDate.getTime() + 15 * 60 * 1000);
+      const newFlight = { ...flight, date: newDate.toISOString() };
+
+      this.store.dispatch(updateFlight({flight: newFlight}));
+    });
   }
 
 }
